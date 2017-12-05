733940a934e9f8f393dee3fa39617ec3fca4873e
Jojo <jojo@pc.com>
2017-12-05 20:57:19 +0800

    made directory changes.

--- FinalProductionCode/DetectionStarter.bat
+++ FinalProductionCode/DetectionStarter.bat
@@ -1,6 +1,10 @@
 @echo off
+:BEGINNING
 start C:\xampped\xampp-control.exe
 PING localhost -n 10 >NUL
 echo Initializing Estrus Monitor...
 python ipcam1.py
+
+echo Press any key to restart the program.
 pause
+goto BEGINNING
